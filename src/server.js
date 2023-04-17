d368a9784edbd527bea05c2a276ebd75b9c60a53
EduViera131 <eduviera369@gmail.com>
2023-04-17 10:01:57 -0300

    Functionality of post method and PORT changes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.post('/user', (request, response) => {
-  response.send('Você chamou o post')
+  const { name, email } = request.body
+
+  response.send(`O seu nome é ${name}, e o seu e-mail é ${email}`)
 })
 
-const PORT = 3000
+const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running in ${PORT}`))
