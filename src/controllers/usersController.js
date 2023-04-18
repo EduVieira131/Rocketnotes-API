012ccfa56afb1452c6e9551058c7371611f031dc
EduViera131 <eduviera369@gmail.com>
2023-04-18 14:41:03 -0300

    cryptography of the password

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,17 +1,28 @@
+const { hash } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 
-const sqliteConnection = require('../database/sqlite') 
+const sqliteConnection = require('../database/sqlite')
 
 class usersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users where email = (?)", [email])
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get(
+      'SELECT * FROM users where email = (?)',
+      [email]
+    )
 
-    if(checkUserExists) {
+    if (checkUserExists) {
       throw new AppError('Esse e-mail já está cadastrado.')
     }
+
+    const hasehdPassword = await hash(password, 8)
+
+    await database.run(
+      'insert into users (name, email, password) values (?, ?, ?)',
+      [name, email, hasehdPassword]
+    )
 
     return response.status(201).json()
   }
